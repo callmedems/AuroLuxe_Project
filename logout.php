a2ee06ab22eeca8c47849d44bc94135652ea6d04
callmedems <[demmi.zepeda@gmail.com]>
2024-11-30 01:47:08 -0600

    updated logout, now to index

--- logout.php
+++ logout.php
@@ -2,5 +2,5 @@
 session_start();
 session_unset();  // Elimina todas las variables de sesión
 session_destroy(); // Destruye la sesión
-header('Location: index.php'); // Redirige al login después de cerrar sesión
+header('Location: index.php'); // Redirige al index después de cerrar sesión
 exit;
